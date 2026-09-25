3a606428c1b58dcfd47c887d2668763b586365cf
InvDog567 <mtwj567@gmail.com>
2026-09-25 11:26:58 +0700

    Updated town

---
+++ Assets/_Win/WalkingP.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class WalkingP : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpHeight = 2f;
+    public float gravity = -20f;
+    public float mouseSensitivity = 2f;
+
+    public Transform cameraTransform;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private float cameraPitch = 0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // Movement
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // Ground check
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        // Mouse look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        cameraPitch -= mouseY;
+        cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0f, 0f);
+    }
+}
